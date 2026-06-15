2f985b8feb22a41bac2f5e9af0cc6d49c856bafc
Jaitej949 <jaitej.g@navtech.io>
2026-06-15 14:44:15 +0530

    changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Successfully deployed the app through github actions- Test 1");
+    res.send("deployed the app through github actions- Test 1");
 });
 
 app.listen(3000, () => {
